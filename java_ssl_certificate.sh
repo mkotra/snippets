9fe0afc27672aee9741a1376fdac3c09bbe5b6c4
Michał <kotra.michal@gmail.com>
2023-01-13 14:31:17 +0100

    Update java_ssl_certificate.sh

--- java_ssl_certificate.sh
+++ java_ssl_certificate.sh
@@ -1,5 +1,7 @@
 # cd to bin directory of jdk 
 
+cd C:\dev\java\jdk-20\bin
+
 ./keytool -importcert -file "C:\dev\code\certificate.cer" -keystore "C:\dev\java\jdk-20\lib\security\cacerts" -alias certificatealias
 
 # type 'yes' to trust certificate. If it prompts for password the default one is 'changeit'
